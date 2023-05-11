e256e90072935910d050c9b0ff81cf84bb2dfa49
= <isom19901122@gmail.com>
2023-05-12 05:18:05 +0900

    Update marketplace.controller.js

--- backend/controllers/marketplace.controller.js
+++ backend/controllers/marketplace.controller.js
@@ -37,6 +37,51 @@ const listForSale = async (req, res, next) => {
     }
 }
 
+
+const getNFTListForSale = async () => {
+    try {
+        const modelSN = uuid.v4();
+        const { projectName, forOnlyUS, about, hasShare, shareName, shareSymbol, maxTotalSupply, mintPrice, mnpw, mnpt, endTime, paymentToken } = req.body;
+
+        if (!hasShare) {
+            return res.status(500).json({
+                status: "error",
+                message: "Use /api/create-basic-project-without-share",
+            });
+        }
+
+        const project = await Project.create({
+            projectName,
+            modelSN,
+            forOnlyUS,
+            about,
+            hasShare,
+            shareName,
+            shareSymbol,
+            maxTotalSupply,
+            mintPrice,
+            mnpt,
+            mnpw,
+            maxTotalSupply,
+            endTime,
+            paymentToken
+        });
+
+        return res.status(201).json({
+            status: "success",
+            data: {
+                project,
+            },
+        });
+    } catch (error) {
+        return res.status(500).json({
+            status: "error",
+            message: error.message,
+        });
+    }
+}
+
 module.exports = {
-    listForSale
+    listForSale,
+    getNFTListForSale
 }
